70e8649ff9150ecca15fd401560529172f6717de
DESKTOP-RO3A9SP\ms <taifurreza042@gmail.com>
2018-06-23 16:18:06 +0600

    DPNCSt2

--- DPNCSt.java
+++ DPNCSt.java
@@ -1,14 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pkg1505042_state;
 
-/**
- *
- * @author ms
- */
-public class DPNCSt {
+
+public class DPNCSt implements VMSt {
+    
+   @Override
+   
+   public String toString () {
+       
+       return "Thanks for using our VM.\nDrinks with change delivered.";
+   }
     
 }
